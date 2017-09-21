1c9bdea5a2d3d5504263588a86f2be09e062544d
jmorte14 <jmorte14@student.aau.dk>
2017-09-21 15:12:57 +0200

    Updated login page

--- augmented-chess/src/app/login/login.component.ts
+++ augmented-chess/src/app/login/login.component.ts
@@ -9,20 +9,34 @@ export class LoginComponent implements OnInit {
   title = "Login portal for Augmented Chess";
   infoLogin = "Log in with a user name";
   infoRegister = "Otherwize register a new user";
-  userName = "";
-  
+  currentUser = "";
+  loginStatus = "";
+  users: string[];
+
   constructor() { }
 
   ngOnInit() {
+    this.users = Array<string>();
   }
 
-  login(user) {
-    this.userName = user;
-    this.loginStatus = " is currently logged in, have fun!";
+  login(user, box) {
+    if (this.users.includes(user)) {
+      this.currentUser = user;
+      this.loginStatus = " is currently logged in, have fun!";
+    } else {
+      this.loginStatus = "No such user exists!";
+      box.value = "";
+    }
   }
 
-  register(user) {
-    this.userName = user;
-    this.loginStatus = " just registered, have fun!";
+  register(user, box) {
+    if (!(this.users.includes(user)) && user !== "") {
+      this.currentUser = user;
+      this.loginStatus = " just registered, have fun!";
+      this.users.push(user);
+      box.value = "";
+    } else if (this.users.includes(user)) {
+      box.value = "";
+    }
   }
 }
