0b863807d6fba30eab72d039e57333ee73421d19
myrionsc <marpht23@gmail.com>
2017-09-28 14:38:37 +0200

    test of dnd done

--- augmented-chess/src/app/queue/queue.component.ts
+++ augmented-chess/src/app/queue/queue.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DropEvent} from "ng2-drag-drop";
 
 @Component({
   selector: 'app-queue',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./queue.component.css']
 })
 export class QueueComponent implements OnInit {
+    items = [
+        {id: 0, name: "Apple", type: "fruit"},
+        {id: 1, name: "Carrot", type: "vegetable"},
+        {id: 2, name: "Orange", type: "fruit"}];
+    droppedItems = [];
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  onItemDrop(e: DropEvent) {
+      // Get the dropped data here
+
+      // console.log(e.dragData);
+      // console.log(this.items.findIndex(i => i.name === e.dragData.name));
+      // console.log(this.items);
+      this.items.splice(this.items.findIndex(i => i.name === e.dragData.name), 1);
+      this.droppedItems.push(e.dragData);
   }
 
 }
