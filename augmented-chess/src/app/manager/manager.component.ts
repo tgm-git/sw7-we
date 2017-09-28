987595321151830fd550b47ce20c497476ca135c
jmorte14 <jmorte14@student.aau.dk>
2017-09-28 15:39:08 +0200

    Feature: Started setup for Army creater

--- augmented-chess/src/app/manager/manager.component.ts
+++ augmented-chess/src/app/manager/manager.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manager.component.css']
 })
 export class ManagerComponent implements OnInit {
+  armies = [
+      {name: "Army 1", bp: 20},
+      {name: "Army 2", bp: 10},
+      {name: "Army 3", bp: 30},
+      {name: "Army 4", bp: 40}
+  ];
+  selectedArmy = {};
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
 
+  createArmy() {
+      this.armies.push({name: "Army " + (this.armies.length + 1).toString(), bp: 0});
+  }
+
+  deleteArmy(a) {
+    if (a) {
+      this.armies.splice(this.armies.indexOf(a), 1);
+    }
+  }
+
+  saveArmy() {
+
+  }
+
+  selectArmy(a) {
+    this.selectedArmy = a;
+  }
 }
