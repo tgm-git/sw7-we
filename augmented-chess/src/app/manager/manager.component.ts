487146c6d29cfd9f93dda4c3c6e8012cc3d5540c
jmorte14 <jmorte14@student.aau.dk>
2017-10-05 13:41:04 +0200

    Added piece array to army

--- augmented-chess/src/app/manager/manager.component.ts
+++ augmented-chess/src/app/manager/manager.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Army} from "../shared/model/army";
+import {Piece} from "../shared/model/piece";
 
 @Component({
   selector: 'app-manager',
@@ -8,29 +9,29 @@ import {Army} from "../shared/model/army";
 })
 export class ManagerComponent implements OnInit {
   armies: Army[] = [
-      {name: "Army 1", bp: 20},
-      {name: "Army 2", bp: 10},
-      {name: "Army 3", bp: 30},
-      {name: "Army 4", bp: 40}
+      {name: "Army 1", bp: 20, pieces: new Array<Piece>()},
+      {name: "Army 2", bp: 10, pieces: new Array<Piece>()},
+      {name: "Army 3", bp: 30, pieces: new Array<Piece>()},
+      {name: "Army 4", bp: 40, pieces: new Array<Piece>()}
   ];
   selectedArmy: Army;
 
   constructor() {
-    this.selectedArmy = this.armies.length === 0 ? new Army("No army selected", 0) : this.armies[0];
+    this.selectedArmy = this.armies.length === 0 ? new Army("No army selected", 0, new Array<Piece>()) : this.armies[0];
   }
 
   ngOnInit() {
   }
 
   createArmy() {
-      this.armies.push({name: "Army " + (this.armies.length + 1).toString(), bp: 0});
+      this.armies.push({name: "Army " + (this.armies.length + 1).toString(), bp: 0, pieces: new Array<Piece>()});
       this.selectedArmy = this.armies[this.armies.length - 1];
   }
 
   deleteArmy(a) {
     if (this.armies.indexOf(a) !== -1) {
       this.armies.splice(this.armies.indexOf(a), 1);
-      this.selectedArmy = new Army("No army selected", 0);
+      this.selectedArmy = new Army("No army selected", 0, new Array<Piece>());
     }
   }
 
--- augmented-chess/src/app/shared/model/army.ts
+++ augmented-chess/src/app/shared/model/army.ts
@@ -1,9 +1,13 @@
+import {Piece} from "./piece";
+
 export class Army {
     name: string;
     bp: number;
+    pieces: Piece[];
 
-    constructor(name: string, bp: number) {
+    constructor(name: string, bp: number, pieces: Piece[]) {
         this.name = name;
         this.bp = bp;
+        this.pieces = pieces;
     }
 }
