b4316b749a084d546823a5e158197dbcc1dc32a7
myrionsc <marpht23@gmail.com>
2017-10-12 15:19:15 +0200

    created properties for armies in game, made pieces on board only draggable when game started

--- augmented-chess/src/app/game/cell/cell.component.ts
+++ augmented-chess/src/app/game/cell/cell.component.ts
@@ -17,23 +17,33 @@ export class CellComponent implements OnInit {
   ngOnInit() {}
 
   onItemDrop(e: DropEvent) {
-      // check if piece arrived from an image or another cell
+      // check if piece arrived from an armybox or another cell
       if (e.dragData.armybox) { // dragdata in this case consist of armybox and piece
           // todo: check if valid setup position
 
           this.cell.piece = e.dragData.piece;
           this.cell.image = e.dragData.piece.image;
+
+          if (e.dragData.piece.colour === "white") {
+              this.game.whiteArmy.push(e.dragData.piece);
+          } else {
+              this.game.blackArmy.push(e.dragData.piece);
+          }
           e.dragData.armybox.splice(e.dragData.armybox.indexOf(e.dragData.piece), 1);
+          this.game.checkSetupDone();
       } else { // dragdata in this case consists of cell
-          // todo: check if valid movement
+          if (this.cell !== e.dragData.cell) {
+              // todo: check if valid movement
+              
 
-          // copy piece to new cell
-          this.cell.image = e.dragData.cell.image;
-          this.cell.piece = Object.assign({}, e.dragData.cell.piece);
+              // copy piece to new cell
+              this.cell.image = e.dragData.cell.image;
+              this.cell.piece = Object.assign({}, e.dragData.cell.piece);
 
-          // remove piece from old cell
-          e.dragData.cell.image = "";
-          e.dragData.cell.piece = null;
+              // remove piece from old cell
+              e.dragData.cell.image = "";
+              e.dragData.cell.piece = null;
+          }
       }
   }
 }
--- augmented-chess/src/app/shared/model/game.ts
+++ augmented-chess/src/app/shared/model/game.ts
@@ -2,6 +2,18 @@ import {Piece} from "./piece";
 
 export class Game {
     phase = "setup"; // "setup" and "play"
-    whitePlacementArmy: Piece[];
-    blackPlacementArmy: Piece[];
+    whitePlacementArmy: Piece[] = [];
+    blackPlacementArmy: Piece[] = [];
+    whiteArmy: Piece[] = [];
+    blackArmy: Piece[] = [];
+
+    
+
+    constructor () {}
+
+    checkSetupDone() {
+        if (this.whitePlacementArmy.length === 0 && this.blackPlacementArmy.length === 0) {
+            this.phase = "play";
+        }
+    }
 }
