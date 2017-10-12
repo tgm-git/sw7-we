9276505cbeb702544645f2003219b372949fceab
myrionsc <marpht23@gmail.com>
2017-10-12 15:41:12 +0200

    added placement restriction when setting pieces on the board in setup phase

--- augmented-chess/src/app/game/cell/cell.component.ts
+++ augmented-chess/src/app/game/cell/cell.component.ts
@@ -20,21 +20,23 @@ export class CellComponent implements OnInit {
       // check if piece arrived from an armybox or another cell
       if (e.dragData.armybox) { // dragdata in this case consist of armybox and piece
           // todo: check if valid setup position
+          if (!this.cell.piece && this.game.validSetupPos(e.dragData.piece.colour, this.cell.pos)) {
+              this.cell.piece = e.dragData.piece;
+              this.cell.image = e.dragData.piece.image;
 
-          this.cell.piece = e.dragData.piece;
-          this.cell.image = e.dragData.piece.image;
-
-          if (e.dragData.piece.colour === "white") {
-              this.game.whiteArmy.push(e.dragData.piece);
-          } else {
-              this.game.blackArmy.push(e.dragData.piece);
+              if (e.dragData.piece.colour === "white") {
+                  this.game.whiteArmy.push(e.dragData.piece);
+              } else {
+                  this.game.blackArmy.push(e.dragData.piece);
+              }
+              e.dragData.armybox.splice(e.dragData.armybox.indexOf(e.dragData.piece), 1);
+              this.game.checkSetupDone();
           }
-          e.dragData.armybox.splice(e.dragData.armybox.indexOf(e.dragData.piece), 1);
-          this.game.checkSetupDone();
+
       } else { // dragdata in this case consists of cell
           if (this.cell !== e.dragData.cell) {
               // todo: check if valid movement
-              
+
 
               // copy piece to new cell
               this.cell.image = e.dragData.cell.image;
--- augmented-chess/src/app/shared/model/game.ts
+++ augmented-chess/src/app/shared/model/game.ts
@@ -1,5 +1,6 @@
 import {Piece} from "./piece";
 import {Pos} from "./pos";
+import {Cell} from "./cell";
 
 export class Game {
     phase = "setup"; // "setup" and "play"
@@ -9,7 +10,12 @@ export class Game {
     blackArmy: Piece[] = [];
 
     whiteStartingPos: Pos[] = [
-
+        new Pos(0, 1), new Pos(1, 1), new Pos(2, 1), new Pos(3, 1), new Pos(4, 1), new Pos(5, 1), new Pos(6, 1), new Pos(7, 1),
+        new Pos(0, 0), new Pos(1, 0), new Pos(2, 0), new Pos(3, 0), new Pos(4, 0), new Pos(5, 0), new Pos(6, 0), new Pos(7, 0),
+    ];
+    blackStartingPos: Pos[] = [
+        new Pos(0, 7), new Pos(1, 7), new Pos(2, 7), new Pos(3, 7), new Pos(4, 7), new Pos(5, 7), new Pos(6, 7), new Pos(7, 7),
+        new Pos(0, 6), new Pos(1, 6), new Pos(2, 6), new Pos(3, 6), new Pos(4, 6), new Pos(5, 6), new Pos(6, 6), new Pos(7, 6)
     ];
 
     constructor () {}
@@ -18,5 +24,11 @@ export class Game {
         if (this.whitePlacementArmy.length === 0 && this.blackPlacementArmy.length === 0) {
             this.phase = "play";
         }
+    }
+    validSetupPos(colour: string, pos: Pos): boolean {
+        if (colour === "white") {
+            return !!this.whiteStartingPos.find(p => p.equal(pos));
+        }
+        return !!this.blackStartingPos.find(p => p.equal(pos));
     }
 }
