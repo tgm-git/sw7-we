7e20e16306a3c246ec67b34c32694694cb8e9673
myrionsc <marpht23@gmail.com>
2017-10-19 12:23:45 +0200

    added notation to cells

--- augmented-chess/src/app/shared/model/cell.ts
+++ augmented-chess/src/app/shared/model/cell.ts
@@ -2,15 +2,17 @@ import {Piece} from "./piece";
 import {Pos} from "./pos";
 
 export class Cell {
+  notation: string;
   pos: Pos;
   backgroundColour: string;
   backgroundColourOriginal: string;
   piece: Piece;
   image = "";
 
-  constructor(posX, posY, backgroundColour) {
+  constructor(posX, posY, backgroundColour, notation) {
     this.pos = new Pos(posX, posY);
     this.backgroundColour = backgroundColour;
     this.backgroundColourOriginal = backgroundColour;
+    this.notation = notation;
   }
 }
